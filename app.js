febcdb70073ee29d6cea090e4fc4e5377715a106
Viduna <122073048+VidunaPX@users.noreply.github.com>
2025-10-05 15:53:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
